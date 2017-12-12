a263f801cec563e209236dcc3439394d2cc10f72
Ioannis Pasmatzis <johnpasma@hotmail.com>
2017-12-12 21:02:55 +0100

    Initial Commit

---
+++ ARFaceDetection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARFaceDetection
+//
+//  Created by Ioannis Pasmatzis on 12/12/17.
+//  Copyright © 2017 Yanniki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
